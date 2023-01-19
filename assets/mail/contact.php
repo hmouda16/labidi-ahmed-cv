cbdb977c675a004002761bb2cba473c6deece1ce
hmouda16 <ahmed-1611@live.fr>
2023-01-19 12:17:02 +0100

    modif2

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -9,8 +9,8 @@ if ($_SERVER["REQUEST METHOD"] == POST) {
 $mailto = 'ahmed-1611@live.fr';
 $sujet = 'Nouveau mail depuis le site internet de bg';
 $mailheader = "From:".$emailClient."\r\n";
-$mailheader = "Reply-To:".$emailClient."\r\n";
-$$mailheader = "Content-type: text/html; charset=UTF-8\r\n";
+$mailheader .= "Reply-To:".$emailClient."\r\n";
+$mailheader .= "Content-type: text/html; charset=UTF-8\r\n";
 $message = "Nouveau mail de " . $nomClient . "\r\n" . "Son adresse mail est : " . $emailClient . "\r\n" . "Son message est : " . $commentairesClient;
 
 //traiter mail
@@ -20,4 +20,4 @@ mail($mailto, $sujet, $message, $mailheader);
 // success
 if ($mail = true) {
     echo "ok";
-}
+};
