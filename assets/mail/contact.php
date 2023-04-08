0d88ebeaedfcc08e7734545879ae81212ef9f761
hmouda16 <ahmed-1611@live.fr>
2023-04-09 00:11:54 +0200

    portfolio

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -1,15 +1,16 @@
+<?php
 // poster reponses dans variables
-if ($_SERVER["REQUEST METHOD"] == POST) {
+if ($_SERVER["REQUEST METHOD"] == "POST") {
     $nomClient = $_POST['name'];
     $emailClient = $_POST['email'];
     $commentairesClient = $_POST['commentaires'];
 };
 
 // traiter messages
-$mailto = 'ahmed-1611@live.fr';
-$sujet = 'Nouveau mail depuis le site internet de bg';
-$mailheader = "From:".$emailClient."\r\n";
-$mailheader .= "Reply-To:".$emailClient."\r\n";
+$mailto = 'hmouda1611@gmail.com';
+$sujet = 'Nouveau mail depuis le site internet de ahmed';
+$mailheader = "From:" . $emailClient . "\r\n";
+$mailheader .= "Reply-To:" . $emailClient . "\r\n";
 $mailheader .= "Content-type: text/html; charset=UTF-8\r\n";
 $message = "Nouveau mail de " . $nomClient . "\r\n" . "Son adresse mail est : " . $emailClient . "\r\n" . "Son message est : " . $commentairesClient;
 
