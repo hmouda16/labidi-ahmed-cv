d5086d54abb0f0db81d4baa74802ecae97dfbac5
hmouda16 <ahmed-1611@live.fr>
2023-04-09 22:51:44 +0200

    integration eperience

--- script.js
+++ script.js
@@ -20,36 +20,50 @@ var typed = new Typed(".typed", {
 });
 
 //compteur
-let compteur = 0;
+// let compteur = 0;
 
-$(window).scroll(function () {
-  const top = ($(".counter").offset().top = window.innerHeight);
+// $(window).scroll(function () {
+//   const top = ($(".counter").offset().top = window.innerHeight);
 
-  if (compteur == 0 && $(window).scrollTop() > top) {
-    $(".counter-value").each(function () {
-      let $this = $(this),
-        countTo = $this.attr("data-count");
-      $({
-        countNum: $this.text(),
-      }).animate(
-        {
-          countNum: countTo,
-        },
-        {
-          duration: 10000,
-          easing: "swing",
-          step: function () {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function () {
-            $this.text(this.countNum);
-          },
-        }
-      );
-    });
-    compteur = 1;
+//   if (compteur == 0 && $(window).scrollTop() > top) {
+//     $(".counter-value").each(function () {
+//       let $this = $(this),
+//         countTo = $this.attr("data-count");
+//       $({
+//         countNum: $this.text(),
+//       }).animate(
+//         {
+//           countNum: countTo,
+//         },
+//         {
+//           duration: 10000,
+//           easing: "swing",
+//           step: function () {
+//             $this.text(Math.floor(this.countNum));
+//           },
+//           complete: function () {
+//             $this.text(this.countNum);
+//           },
+//         }
+//       );
+//     });
+//     compteur = 1;
+//   }
+// });
+
+
+/*EXPERIENCE*/
+
+window.addEventListener("scroll", function() {
+  var section = document.getElementById("experience");
+  var position = section.getBoundingClientRect();
+
+  /* Si la section est dans la vue, ajoutez la classe d'animation */
+  if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    section.classList.add("fadeIn");
   }
 });
+
 
 /*********************** AOS *****************/
 AOS.init();
